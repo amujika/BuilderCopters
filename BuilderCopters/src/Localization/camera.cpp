7835266f8337f815035b6960e19438c16e8f0e12
amujika <amujika99@gmail.com>
2014-06-10 23:31:22 +0200

    Camera calibration using HSV filters

--- BuilderCopters/src/Localization/camera.cpp
+++ BuilderCopters/src/Localization/camera.cpp
@@ -12,34 +12,46 @@ Camera::Camera() {
 Camera::Camera(string name, int port) {
 	name_ = name;
 	video_input_ = VideoCapture(port);
+
+	LowH = 0;
+	HighH = 179;
+	LowS = 0;
+	HighS = 255;
+	LowV = 0;
+	HighV = 255;
 }
 
 void Camera::calibrateColorFilters() {
-	Mat color_frame, HSV_frame, threshold_frame;
-	string window_name = "calibrate camera: " + this->name_;
+	Mat RGB_frame, HSV_frame, threshold_frame;
+	string calibrate_window_name = "calibrate camera: " + name_;
+	string original_window_name = "original image: " + name_;
 
-	namedWindow(window_name);
+	namedWindow(calibrate_window_name);
+	namedWindow(original_window_name);
 
 	//Trackbars for the user to calibrate the threshold
-	createTrackbar("LowH", window_name, &LowH, 179); //Hue (0 - 179)
-	createTrackbar("HighH", window_name, &HighH, 179);
-	createTrackbar("LowS", window_name, &LowS, 255); //Saturation (0 - 255)
-	createTrackbar("HighS", window_name, &HighS, 255);
-	createTrackbar("LowV", window_name, &LowV, 255); //Value (0 - 255)
-	createTrackbar("HighV", window_name, &HighV, 255);
+	createTrackbar("LowH", calibrate_window_name, &LowH, 179); //Hue (0 - 179)
+	createTrackbar("HighH", calibrate_window_name, &HighH, 179);
+	createTrackbar("LowS", calibrate_window_name, &LowS, 255); //Saturation (0 - 255)
+	createTrackbar("HighS", calibrate_window_name, &HighS, 255);
+	createTrackbar("LowV", calibrate_window_name, &LowV, 255); //Value (0 - 255)
+	createTrackbar("HighV", calibrate_window_name, &HighV, 255);
 
 	while ((char)waitKey(15) != 'x') {
 		// @@TODO. This loops video files, when using webcams replace with (video_input_ >> frame;)
-		if (!video_input_.read(color_frame)) {
+		if (!video_input_.read(RGB_frame)) {
 			video_input_.set(CV_CAP_PROP_POS_FRAMES, 0);
 			continue;
 		}
-		cvtColor(color_frame, HSV_frame, CV_RGB2HSV);
-		inRange(HSV_frame, Scalar(100, 100, 100), Scalar(120, 120, 120), threshold_frame);
 
-		imshow(window_name, threshold_frame);
+		cvtColor(RGB_frame, HSV_frame, CV_RGB2HSV);
+		inRange(HSV_frame, Scalar(LowH, LowS, LowV), Scalar(HighH, HighS, HighV), threshold_frame);
+
+		imshow(calibrate_window_name, threshold_frame);
+		imshow(original_window_name, RGB_frame);
 	}
-	destroyWindow(window_name);
+	destroyWindow(calibrate_window_name);
+	destroyWindow(original_window_name);
 }
 
 bool Camera::isVideoOpened() {
